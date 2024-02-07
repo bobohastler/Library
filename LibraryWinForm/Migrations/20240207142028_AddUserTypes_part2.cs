ecb69d96aa95fc759f318c7f2c4507fe0c0f939b
andrikonon <andri.konon@gmail.com>
2024-02-07 15:22:07 +0100

    Performed migrations, fixed problems.

---
+++ LibraryWinForm/Migrations/20240207142028_AddUserTypes_part2.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LibraryWinForm.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUserTypes_part2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "tblCategories",
+                columns: table => new
+                {
+                    CategoryId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: false),
+                    DateCreated = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_tblCategories", x => x.CategoryId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "tblUserTypes",
+                columns: table => new
+                {
+                    UserTypeId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_tblUserTypes", x => x.UserTypeId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "tblBooks",
+                columns: table => new
+                {
+                    BookId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(4000)", maxLength: 4000, nullable: false),
+                    AuthorName = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                    Pages = table.Column<int>(type: "int", nullable: false),
+                    Count = table.Column<int>(type: "int", nullable: false),
+                    CategoryId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_tblBooks", x => x.BookId);
+                    table.ForeignKey(
+                        name: "FK_tblBooks_tblCategories_CategoryId",
+                        column: x => x.CategoryId,
+                        principalTable: "tblCategories",
+                        principalColumn: "CategoryId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "tblUsers",
+                columns: table => new
+                {
+                    UserId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: false),
+                    Phone = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false),
+                    DateCreated = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UserTypeId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_tblUsers", x => x.UserId);
+                    table.ForeignKey(
+                        name: "FK_tblUsers_tblUserTypes_UserTypeId",
+                        column: x => x.UserTypeId,
+                        principalTable: "tblUserTypes",
+                        principalColumn: "UserTypeId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_tblBooks_CategoryId",
+                table: "tblBooks",
+                column: "CategoryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_tblUsers_UserTypeId",
+                table: "tblUsers",
+                column: "UserTypeId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "tblBooks");
+
+            migrationBuilder.DropTable(
+                name: "tblUsers");
+
+            migrationBuilder.DropTable(
+                name: "tblCategories");
+
+            migrationBuilder.DropTable(
+                name: "tblUserTypes");
+        }
+    }
+}
