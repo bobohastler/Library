8946b6da92c83ab895d4b2c7cf7f7c69c195a355
kazzutora <diabloroale9@gmail.com>
2024-02-18 00:05:42 +0200

    Створив форму та додав кнопки

--- LibraryWinForm/FormValidePurchase.Designer.cs
+++ LibraryWinForm/FormValidePurchase.Designer.cs
@@ -28,12 +28,97 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "FormValidePurchase";
+            button1 = new Button();
+            button2 = new Button();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            dataGridView1 = new DataGridView();
+            numericUpDown1 = new NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).BeginInit();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(710, 114);
+            button1.Name = "button1";
+            button1.Size = new Size(187, 54);
+            button1.TabIndex = 0;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(723, 372);
+            button2.Name = "button2";
+            button2.Size = new Size(187, 50);
+            button2.TabIndex = 1;
+            button2.Text = "button2";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(103, 89);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(296, 27);
+            textBox1.TabIndex = 2;
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(103, 188);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(296, 27);
+            textBox2.TabIndex = 3;
+            textBox2.TextChanged += textBox2_TextChanged;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(103, 325);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 51;
+            dataGridView1.Size = new Size(300, 188);
+            dataGridView1.TabIndex = 4;
+            dataGridView1.CellContentClick += dataGridView1_CellContentClick;
+            // 
+            // numericUpDown1
+            // 
+            numericUpDown1.Location = new Point(437, 129);
+            numericUpDown1.Name = "numericUpDown1";
+            numericUpDown1.Size = new Size(150, 27);
+            numericUpDown1.TabIndex = 5;
+            numericUpDown1.ValueChanged += numericUpDown1_ValueChanged;
+            // 
+            // FormValidePurchase
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1040, 481);
+            Controls.Add(numericUpDown1);
+            Controls.Add(dataGridView1);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Name = "FormValidePurchase";
+            Text = "FormValidePurchase";
+            Load += FormValidePurchase_Load;
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button button1;
+        private Button button2;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private DataGridView dataGridView1;
+        private NumericUpDown numericUpDown1;
     }
 }
--- LibraryWinForm/FormValidePurchase.cs
+++ LibraryWinForm/FormValidePurchase.cs
@@ -12,9 +12,80 @@ namespace LibraryWinForm
 {
     public partial class FormValidePurchase : Form
     {
+        private readonly List<Product> cartProducts;
+
         public FormValidePurchase()
         {
             InitializeComponent();
+            cartProducts = new List<Product>();
+        }
+
+
+
+        // Клас для представлення товару
+        public class Product
+        {
+            public string Name { get; set; }
+            public decimal Price { get; set; }
+            public int Quantity { get; set; }
+        }
+
+
+        private void FormValidePurchase_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+           
+            MessageBox.Show("Замовлення підтверджено!");
+            //Треба код для відправлення замовлення в базу даних 
+
+            // Очистка кошика після підтвердження замовлення
+            cartProducts.Clear();
+            DisplayCartProducts();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Отримання інформації про вибраний товар
+            string productName = textBox1.Text;
+            decimal productPrice = numericUpDown1.Value;
+            int productQuantity = (int)numericUpDown1.Value;
+
+            // Додавання товару в кошик
+            cartProducts.Add(new Product { Name = productName, Price = productPrice, Quantity = productQuantity });
+
+            // Відображення товарів у кошику
+            DisplayCartProducts();
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            // Ваш код обробки зміни значення в числовому полі numericUpDown1
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+           
+        }
+        private void DisplayCartProducts()
+        {
+            dataGridView1.Rows.Clear();
+            foreach (var product in cartProducts)
+            {
+                dataGridView1.Rows.Add(product.Name, product.Price, product.Quantity, product.Price * product.Quantity);
+            }
         }
     }
 }
