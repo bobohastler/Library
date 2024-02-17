8601b1ae062b27a735b3b5a134176cf2a2177db6
kazzutora <diabloroale9@gmail.com>
2024-02-17 23:26:44 +0200

    1.Добавив кнопкі

--- LibraryWinForm/FormValidePurchase.Designer.cs
+++ LibraryWinForm/FormValidePurchase.Designer.cs
@@ -28,12 +28,98 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "FormValidePurchase";
+            txtProductName = new TextBox();
+            txtProductPrice = new TextBox();
+            btnAddToCart = new Button();
+            numericUpDownQuantity = new NumericUpDown();
+            DisplayCartProducts = new DataGridView();
+            btnConfirmOrder = new Button();
+            ((System.ComponentModel.ISupportInitialize)numericUpDownQuantity).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)DisplayCartProducts).BeginInit();
+            SuspendLayout();
+            // 
+            // txtProductName
+            // 
+            txtProductName.Location = new Point(81, 69);
+            txtProductName.Name = "txtProductName";
+            txtProductName.Size = new Size(168, 27);
+            txtProductName.TabIndex = 0;
+            txtProductName.TextChanged += textBox1_TextChanged;
+            // 
+            // txtProductPrice
+            // 
+            txtProductPrice.Location = new Point(81, 157);
+            txtProductPrice.Name = "txtProductPrice";
+            txtProductPrice.Size = new Size(168, 27);
+            txtProductPrice.TabIndex = 1;
+            // 
+            // btnAddToCart
+            // 
+            btnAddToCart.BackColor = SystemColors.ActiveCaption;
+            btnAddToCart.ForeColor = SystemColors.ControlText;
+            btnAddToCart.Location = new Point(550, 100);
+            btnAddToCart.Name = "btnAddToCart";
+            btnAddToCart.Size = new Size(166, 58);
+            btnAddToCart.TabIndex = 2;
+            btnAddToCart.Text = "Додати товар";
+            btnAddToCart.UseVisualStyleBackColor = false;
+            btnAddToCart.Click += button1_Click;
+            // 
+            // numericUpDownQuantity
+            // 
+            numericUpDownQuantity.Location = new Point(290, 117);
+            numericUpDownQuantity.Name = "numericUpDownQuantity";
+            numericUpDownQuantity.Size = new Size(150, 27);
+            numericUpDownQuantity.TabIndex = 3;
+            numericUpDownQuantity.ValueChanged += numericUpDownQuantity_ValueChanged;
+            // 
+            // DisplayCartProducts
+            // 
+            DisplayCartProducts.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            DisplayCartProducts.Location = new Point(41, 261);
+            DisplayCartProducts.Name = "DisplayCartProducts";
+            DisplayCartProducts.RowHeadersWidth = 51;
+            DisplayCartProducts.Size = new Size(327, 148);
+            DisplayCartProducts.TabIndex = 4;
+            DisplayCartProducts.CellContentClick += dataGridView1_CellContentClick;
+            // 
+            // btnConfirmOrder
+            // 
+            btnConfirmOrder.BackColor = SystemColors.ActiveCaption;
+            btnConfirmOrder.Location = new Point(548, 290);
+            btnConfirmOrder.Name = "btnConfirmOrder";
+            btnConfirmOrder.Size = new Size(168, 50);
+            btnConfirmOrder.TabIndex = 5;
+            btnConfirmOrder.Text = "Підтвердити замовлення";
+            btnConfirmOrder.UseVisualStyleBackColor = false;
+            btnConfirmOrder.Click += button1_Click_1;
+            // 
+            // FormValidePurchase
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btnConfirmOrder);
+            Controls.Add(DisplayCartProducts);
+            Controls.Add(numericUpDownQuantity);
+            Controls.Add(btnAddToCart);
+            Controls.Add(txtProductPrice);
+            Controls.Add(txtProductName);
+            Name = "FormValidePurchase";
+            Text = "FormValidePurchase";
+            ((System.ComponentModel.ISupportInitialize)numericUpDownQuantity).EndInit();
+            ((System.ComponentModel.ISupportInitialize)DisplayCartProducts).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private TextBox txtProductName;
+        private TextBox txtProductPrice;
+        private Button btnAddToCart;
+        private NumericUpDown numericUpDownQuantity;
+        private DataGridView DisplayCartProducts;
+        private Button btnConfirmOrder;
     }
 }
--- LibraryWinForm/MainForm.Designer.cs
+++ LibraryWinForm/MainForm.Designer.cs
@@ -28,12 +28,90 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            button1 = new Button();
+            button2 = new Button();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            numericUpDown1 = new NumericUpDown();
+            dataGridView1 = new DataGridView();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(611, 94);
+            button1.Name = "button1";
+            button1.Size = new Size(94, 29);
+            button1.TabIndex = 0;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(611, 275);
+            button2.Name = "button2";
+            button2.Size = new Size(94, 29);
+            button2.TabIndex = 1;
+            button2.Text = "button2";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(101, 60);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(125, 27);
+            textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(101, 146);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(125, 27);
+            textBox2.TabIndex = 3;
+            // 
+            // numericUpDown1
+            // 
+            numericUpDown1.Location = new Point(278, 109);
+            numericUpDown1.Name = "numericUpDown1";
+            numericUpDown1.Size = new Size(150, 27);
+            numericUpDown1.TabIndex = 4;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(101, 229);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 51;
+            dataGridView1.Size = new Size(300, 188);
+            dataGridView1.TabIndex = 5;
+            // 
+            // MainForm
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(dataGridView1);
+            Controls.Add(numericUpDown1);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Name = "MainForm";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button button1;
+        private Button button2;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private NumericUpDown numericUpDown1;
+        private DataGridView dataGridView1;
     }
 }
